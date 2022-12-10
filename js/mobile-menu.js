4be2ee62da1123fe128fd868c2b798c405eeee2a
AleksandrEmchenko <aleksandremchenko@gmail.com>
2022-12-10 14:29:47 +0200

    add mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,27 @@
 (() => {
-	  const mobileMenu = document.querySelector('.js-menu-container');
-	  const openMenuBtn = document.querySelector('.js-open-menu');
-	  const closeMenuBtn = document.querySelector('.js-close-menu');
-	
-	  const toggleMenu = () => {
-	    const isMenuOpen =
-	      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-	    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-	    mobileMenu.classList.toggle('is-open');
-	
-	    const scrollLockMethod = !isMenuOpen
-	      ? 'disableBodyScroll'
-	      : 'enableBodyScroll';
-	    bodyScrollLock[scrollLockMethod](document.body);
-	  };
-	
-	  openMenuBtn.addEventListener('click', toggleMenu);
-	  closeMenuBtn.addEventListener('click', toggleMenu);
-	
-	  // Close the mobile menu on wider screens if the device orientation changes
-	  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-	    if (!e.matches) return;
-	    mobileMenu.classList.remove('is-open');
-	    openMenuBtn.setAttribute('aria-expanded', false);
-	    bodyScrollLock.enableBodyScroll(document.body);
-	  });
-	})();
+  const refs = {
+    openMobilelBtn: document.querySelector('[data-mobile-open]'),
+    closeMobileBtn: document.querySelector('[data-mobile-close]'),
+    menu: document.querySelector('[data-mobile]'),
+    body: document.querySelector('body'),
+    mobileList: document.querySelector('.menuList')
+  };
+  
+
+  refs.openMobileBtn.addEventListener('click', toggleMobile);
+  refs.closeMobileBtn.addEventListener('click', toggleMobile);
+  refs.closeMobileBtn.addEventListener('click', removeMenu);
+
+  function toggleMobile() {
+    // document.body.classList.toggle('mobile-open');
+    refs.mobile.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+    
+  }
+
+  function removeMenu() {
+    refs.mobile.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
+    
+  }
+})();
